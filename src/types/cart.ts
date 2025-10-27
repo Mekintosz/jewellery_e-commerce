37ba2d1aa67cbd71fc791d862924739bf92b57d9
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-10-27 21:27:28 +0100

    Add cart types

---
+++ src/types/cart.ts
@@ -0,0 +1,26 @@
+import { ProductVariantOption } from './product';
+
+export type CartItem = {
+  productId: string;
+  name: string;
+  price: number;
+  salePrice?: number | null;
+  quantity: number;
+  image: string;
+  variant: ProductVariantOption;
+  maxQuantity: number;
+};
+
+export type CartSummary = {
+  subtotal: number;
+  discounts: number;
+  tax: number;
+  total: number;
+};
+
+export type Coupon = {
+  code: string;
+  description: string;
+  discountPercentage: number;
+  expiresAt?: string;
+};
