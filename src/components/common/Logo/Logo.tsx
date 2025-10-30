97a19114a243ef5e1c5d4ec1ad728beb37a72b4a
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-10-30 20:39:33 +0100

    Add Logo component

---
+++ src/components/common/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import styles from "./Logo.module.css";
+import { Link } from "react-router-dom";
+
+type LogoProps = {
+  to?: string;
+};
+
+export const Logo = ({ to = "/" }: LogoProps) => (
+  <Link to={to} className={styles.logo} aria-label="Jewellery home">
+    <span className={styles["logo__text"]}>Jewells and gold</span>
+  </Link>
+);
